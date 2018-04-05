f057c91300ac9c1e981bb6029385f5069c557672
Sandeep100899 <37381008+Sandeep100899@users.noreply.github.com>
2018-04-05 22:35:00 +0530

    Update Project4.c

--- Project4.c
+++ Project4.c
@@ -4,5 +4,20 @@
 
 int main()
 {
+  int n;
+	printf("Enter number of process:");
+	scanf("%d", &n);
+	int queue1[n], queue2[n];
+	int bt[n], at[n], pr[n], rbt[n];
+	printf("\nEnter burst time, priority and arrival time of \n");
+	for(i = 0; i<n ; i++){
+		printf("process %d:" ,i+1);
+		scanf("%d%d%d", &bt[i], &pr[i], &at[i]);
+		printf("\n");
+	}
+	for(i = 0; i<n ; i++)
+	{
+		rbt[i] = bt[i];
+	}
 
 }
